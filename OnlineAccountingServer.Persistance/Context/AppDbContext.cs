04e899f7f4a03d1b4fe044d70801ccc7622af5f1
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:47:34 +0300

    Main Database Context class was created for the application.

---
+++ OnlineAccountingServer.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineAccountingServer.Domain.AppEntities;
+using OnlineAccountingServer.Domain.AppEntities.Identity;
+
+namespace OnlineAccountingServer.Persistance.Context
+{
+    public sealed class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<UserAndCompanyRelationship> UserAndCompanyRelationships { get; set; }
+        
+
+    }
+}
