47dfbae3fb6d18af99b61f69081a7fc3b949e3d3
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:57:10 +0300

    AssemblyReference class has been added in order not to write DbSet property one by one. (Druing OnModelCreating)

---
+++ OnlineAccountingServer.Persistance/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace OnlineAccountingServer.Persistance
+{
+    public static class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
