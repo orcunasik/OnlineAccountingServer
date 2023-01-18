3d8c02e112c1599ebb2c349d46f58dff39a63437
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:46:40 +0300

    Entity was created for Company.

---
+++ OnlineAccountingServer.Domain/CompanyEntities/UniformChartOfAccount.cs
@@ -0,0 +1,12 @@
+﻿using OnlineAccountingServer.Domain.Abstractions;
+
+namespace OnlineAccountingServer.Domain.CompanyEntities
+{
+    public sealed class UniformChartOfAccount :BaseEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public char Type { get; set; }
+        public string CompanyId { get; set; }
+    }
+}
