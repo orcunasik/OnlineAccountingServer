8f66f24c931d0470572a21acf6ad86239018a847
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:43:24 +0300

    Created was classes for the identity mechanism.

---
+++ OnlineAccountingServer.Domain/AppEntities/Identity/AppRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineAccountingServer.Domain.AppEntities.Identity
+{
+    public sealed class AppRole : IdentityRole<string>
+    {
+    }
+}
---
+++ OnlineAccountingServer.Domain/AppEntities/Identity/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineAccountingServer.Domain.AppEntities.Identity
+{
+    public sealed class AppUser : IdentityUser<string>
+    {
+        
+    }
+}
