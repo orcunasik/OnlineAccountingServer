7d5e2319309dfc7020ae7f1506838ef67764f306
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:52:14 +0300

    Configuration class has been created for UniformChartOfAccount class.

---
+++ OnlineAccountingServer.Persistance/Configuration/UCOAConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineAccountingServer.Domain.CompanyEntities;
+using OnlineAccountingServer.Persistance.Constants;
+
+namespace OnlineAccountingServer.Persistance.Configuration
+{
+    public sealed class UCOAConfiguration : IEntityTypeConfiguration<UniformChartOfAccount>
+    {
+        public void Configure(EntityTypeBuilder<UniformChartOfAccount> builder)
+        {
+            builder.ToTable(TableNames.UniformChartOfAccounts);
+            builder.HasKey(i => i.Id);
+
+        }
+    }
+}
