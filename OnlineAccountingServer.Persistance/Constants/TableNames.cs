bbea85c333a6369e8114f46f0d9c4eb9c1d14771
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:51:01 +0300

    A static class has been created by fluenapi to us use in the ToTable method.

---
+++ OnlineAccountingServer.Persistance/Constants/TableNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAccountingServer.Persistance.Constants
+{
+    public static class TableNames
+    {
+        public static string UniformChartOfAccounts = nameof(UniformChartOfAccounts);
+    }
+}
