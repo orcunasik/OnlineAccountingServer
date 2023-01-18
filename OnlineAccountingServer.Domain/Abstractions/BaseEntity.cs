14d34cbc846d15424f9896f478f0df2474765dc0
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:37:30 +0300

    Added BaseEntity abstract classed

---
+++ OnlineAccountingServer.Domain/Abstractions/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineAccountingServer.Domain.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
