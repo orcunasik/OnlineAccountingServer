b3aa0b93ea785f2ae2f5fdc1e15f9578a4c2fc93
Orçun Aşık <asikorcun@gmail.com>
2023-01-18 17:45:31 +0300

    Created was Entities and the relational structure has been established.

---
+++ OnlineAccountingServer.Domain/AppEntities/Company.cs
@@ -0,0 +1,19 @@
+﻿using OnlineAccountingServer.Domain.Abstractions;
+
+namespace OnlineAccountingServer.Domain.AppEntities
+{
+    public sealed class Company : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string IdentityNumber { get; set; }
+        public string TaxDepartment { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ OnlineAccountingServer.Domain/AppEntities/UserAndCompanyRelationship.cs
@@ -0,0 +1,17 @@
+﻿using OnlineAccountingServer.Domain.Abstractions;
+using OnlineAccountingServer.Domain.AppEntities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineAccountingServer.Domain.AppEntities
+{
+    public class UserAndCompanyRelationship : BaseEntity
+    {
+        [ForeignKey("AppUser")]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        [ForeignKey("Company")]
+        public string CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
